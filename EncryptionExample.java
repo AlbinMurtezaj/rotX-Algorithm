723f62a222a811d57a2a724446ec7cf9bd68e28e
AlbinaKuleta <116918514+AlbinaKuleta@users.noreply.github.com>
2023-04-16 21:05:23 +0200

    Shtim i komenteve dhe rishikim

--- EncryptionExample.java
+++ EncryptionExample.java
@@ -2,12 +2,9 @@ import java.util.Base64;
 import java.util.Scanner;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-
 public class EncryptionExample {
-
     private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
-    private static final String ROT15_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+    private static final String ROTX_ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static String encrypt(String message, String secretKey) throws Exception {
         SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
         Cipher cipher = Cipher.getInstance(ALGORITHM);
@@ -15,12 +12,31 @@ public class EncryptionExample {
         byte[] encryptedBytes = cipher.doFinal(message.getBytes("UTF-8"));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
-     public static String rot15Encrypt(String message) {
+    public static String decrypt(String encryptedMessage, String secretKey) throws Exception {
+        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+        return new String(decryptedBytes, "UTF-8");
+    }
+    public static String rotXEncrypt(String message) {
         StringBuilder result = new StringBuilder();
         for (char c : message.toCharArray()) {
-            if (ROT15_ALPHABET.indexOf(c) >= 0) {
-                int index = (ROT15_ALPHABET.indexOf(c) + 15) % ROT15_ALPHABET.length();
-                result.append(ROT15_ALPHABET.charAt(index));
+            if (ROTX_ALPHABET.indexOf(c) >= 0) {
+                int index = (ROTX_ALPHABET.indexOf(c) + 15) % ROTX_ALPHABET.length();
+                result.append(ROTX_ALPHABET.charAt(index));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+    public static String rotXDecrypt(String encryptedMessage) {
+        StringBuilder result = new StringBuilder();
+        for (char c : encryptedMessage.toCharArray()) {
+            if (ROTX_ALPHABET.indexOf(c) >= 0) {
+                int index = (ROTX_ALPHABET.indexOf(c) - 15 + ROTX_ALPHABET.length()) % ROTX_ALPHABET.length();
+                result.append(ROTX_ALPHABET.charAt(index));
             } else {
                 result.append(c);
             }
@@ -28,68 +44,49 @@ public class EncryptionExample {
         return result.toString();
     }
 
-    public static String rot15Decrypt(String encryptedMessage) {
-        StringBuilder result = new StringBuilder();
-        for (char c : encryptedMessage.toCharArray()) {
-            if (ROT15_ALPHABET.indexOf(c) >= 0) {
-                int index = (ROT15_ALPHABET.indexOf(c) - 15 + ROT15_ALPHABET.length()) % ROT15_ALPHABET.length();
-                result.append(ROT15_ALPHABET.charAt(index));
-            } else {
-                result.append(c);
-            }
-        }
-        return result.toString();
-    }
-    
-    
-    
-     public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Vendose tekstin: ");
         String message = scanner.nextLine();
         String secretKey;
         while(true) {
-        	System.out.print("Vendos gjatësin e çelsit(16, 24 ose 32 bajt :");
-        	secretKey=(scanner.nextLine());
-        	if(secretKey.length()== 16 || secretKey.length() == 24 || secretKey.length() == 32) {
-        		break;
-        	}
-        	System.out.println("Gjatesia e çelsit nuk eshte e sakt. Ju lutem provoni perseri!");
+            System.out.print("Vendos gjatësine e çelesit(16, 24 ose 32 bajt :");
+            secretKey=(scanner.nextLine());
+            if(secretKey.length()== 16 || secretKey.length() == 24 || secretKey.length() == 32) {
+                break;
+            }
+            System.out.println("Gjatesia e çelesit nuk eshte e sakte. Ju lutem provoni perseri!");
         }
-        
-        
+
+
         // Enkriptimi simetrik
         String encryptedMessage = encrypt(message, secretKey);
         System.out.println("Mesazhi i enkriptuar: " + encryptedMessage);
-
-        // ne qofte se perdoruesi pergjigjet me po, athere mesazhi i enkriptuar, dekriptohet dhe shfaqet
+        // ne qofte se perdoruesi pergjigjet me po, atehere mesazhi i enkriptuar, dekriptohet pastaj shfaqet
         while (true) {
-            System.out.print("A deshiron qe te shfaqet mesazhi i dekriptuar: (P/J): ");
+            System.out.print("A deshiron qe te shfaqet mesazhi i dekriptuar? (P/J): ");
             String choice = scanner.nextLine();
-
-            // kontrollon neser perdoruese e shtyp "P" ose "J"
+            // kontrollon nese perdoruesi e shtype "P" ose "J"
             if (choice.equalsIgnoreCase("P")) {
                 String decryptedMessage = decrypt(encryptedMessage, secretKey);
                 System.out.println("Mesazhi i dekriptuar: " + decryptedMessage);
-                break; // del nga loop-a nese e shtypa vlera valide
+                break; // del nga loop-a nese shtype vlera valide
             } else if (choice.equalsIgnoreCase("J")) {
-                break; // del nga loop-a nese e shtypa vlera valide
+                break; // del nga loop-a nese e shtyp vlera valide
             } else {
                 System.out.println("Opsioni i zgjedhur nuk eshte valid. Ju lutem provoni perseri me P ose J.");
             }
         }
-         
-        //cakton sa do te jete rotacioni 
+
+        //cakton sa do te jete rotacioni
         System.out.print("Vendose sa deshironi te jete rotacioni X=");
         int rotX = Integer.parseInt(scanner.nextLine());
         //Enkriptimi rotX
         String encryptedMessageRotX = rotXEncrypt(message);
         System.out.println("Mesazhi enkriptuar me ROT" + rotX + ": " + encryptedMessageRotX);
-
         String decryptedMessageRotX = rotXDecrypt(encryptedMessageRotX);
         System.out.println("Mesazhi denkriptuar me ROT" + rotX + ": " + decryptedMessageRotX);
-
-
         scanner.close();
     }
 }

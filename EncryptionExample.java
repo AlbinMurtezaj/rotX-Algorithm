826055985c7a3927008cf9c0b6df68998c248f64
AlbinMurtezaj <122055604+AlbinMurtezaj@users.noreply.github.com>
2023-04-16 15:08:20 +0200

    Update EncryptionExample.java

--- EncryptionExample.java
+++ EncryptionExample.java
@@ -15,5 +15,46 @@ public class EncryptionExample {
         byte[] encryptedBytes = cipher.doFinal(message.getBytes("UTF-8"));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
+     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Vendose tekstin: ");
+        String message = scanner.nextLine();
+        System.out.print("Vendose çelsin: ");
+        String secretKey = scanner.nextLine();
+
+        // Enkriptimi simetrik
+        String encryptedMessage = encrypt(message, secretKey);
+        System.out.println("Mesazhi i enkriptuar: " + encryptedMessage);
+
+        // i ofron perdorusit te zgjedh nese deshiron ta shoh ne ekran mesazhin e dekriptuar
+        System.out.print("A deshiron qe te shfaqet mesazhi i dekriptuar: (P/J): ");
+        String choice = scanner.nextLine();
+
+        // ne qofte se perdoruesi pergjigjet me po, athere mesazhi i enkriptuar, dekriptohet dhe shfaqet
+        while (true) {
+            System.out.print("A deshiron qe te shfaqet mesazhi i dekriptuar: (P/J): ");
+            choice = scanner.nextLine();
+
+            // kontrollon neser perdoruese e shtyp "P" ose "J"
+            if (choice.equalsIgnoreCase("P")) {
+                String decryptedMessage = decrypt(encryptedMessage, secretKey);
+                System.out.println("Mesazhi i dekriptuar: " + decryptedMessage);
+                break; // del nga loop-a nese e shtypa vlera valide
+            } else if (choice.equalsIgnoreCase("J")) {
+                break; // del nga loop-a nese e shtypa vlera valide
+            } else {
+                System.out.println("Opsioni i zgjedhur nuk eshte valid. Ju lutem provoni perseri me P ose J.");
+            }
+        }
+        //Enkriptimi rot15
+        String encryptedMessageRot15 = rot15Encrypt(message);
+        System.out.println("Mesazhi enkriptuar me ROT15: " + encryptedMessageRot15);
+
+        String decryptedMessageRot15 = rot15Decrypt(encryptedMessageRot15);
+        System.out.println("Mesazhi denkriptuar me ROT15: " + decryptedMessageRot15);
+
+        scanner.close();
+    }
 }
+
 
